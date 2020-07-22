4842831203653a666368b1f4fe3f253ef714fe30
faiz <faizrahimbusiness@gmail.com>
2020-07-22 18:02:08 +0530

    created a prefixHandler in utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import aiohttp
 import configparser
 from bs4 import BeautifulSoup
 from discord import Embed
+from typing import Union
+import discord
+import pandas as pd
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -127,3 +130,75 @@ def convert_from_arabic_number(number_string):
      ':': ':'
     }
     return "".join([dic[char] for char in number_string])
+
+
+
+
+path = "prefixes.csv"
+
+def make_csv():
+    df = pd.DataFrame(columns=['guildID', 'prefix', 'authorID'])
+    df.to_csv(path, index=False)
+
+
+def get_csv():
+    try:
+        pd.read_csv(path)
+    except FileNotFoundError:
+        make_csv()
+
+    df = pd.read_csv(path)
+    return df
+
+
+class PrefixHandler:
+    df = get_csv()
+
+    @classmethod
+    def save(cls):
+        cls.df.to_csv(path, index=False)
+
+    @classmethod
+    def add_prefix(cls, author: discord.Member, guild_id: int, prefix: str):
+        if guild_id not in cls.df.guildID.values:
+
+            new_row = {
+                "guildID": guild_id,
+                "prefix": prefix,
+                "authorID": author.id
+            }
+
+            cls.df = cls.df.append(new_row, ignore_index=True)
+            cls.save()
+        else:
+            guild_row = cls.df[cls.df.guildID == guild_id]
+            guild_row.prefix.values[0] = prefix
+            cls.df[cls.df.guildID == guild_id] = guild_row
+            cls.save()
+
+    @classmethod
+    def remove_prefix(cls,guild_id : int):
+        if guild_id not in cls.df.guildID.values:
+            pass
+        else:
+
+            cls.df = cls.df[cls.df.guildID != guild_id]
+            cls.save()
+
+
+
+    @classmethod
+    def get_prefix(cls, guild_id: int) -> Union[str, None]:
+        prefix = None
+
+        try:
+            guild_row = cls.df[cls.df.guildID == guild_id]
+            prefix = guild_row.prefix.values[0]
+        except:
+            pass
+
+        return prefix
+
+    @classmethod
+    def has_custom_prefix(cls, guild_id: int) -> bool:
+        return guild_id in cls.df.guildID.values
