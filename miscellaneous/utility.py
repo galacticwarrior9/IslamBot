17368d71dda42ff14db741c935d5c3f72ca1b5dc
galacticwarrior9 <zareef1@yahoo.com>
2021-04-07 16:16:00 +0100

    Add check to cog reload command

--- miscellaneous/utility.py
+++ miscellaneous/utility.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
+from discord.ext.commands import CheckFailure
 
 
 class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.is_owner()
     @commands.command(name='ireload')
     async def reload(self, ctx, cog_name: str):
         if cog_name in self.bot.extensions.keys():
@@ -12,6 +14,11 @@ class Utility(commands.Cog):
             await self.bot.reload_extension(cog_name)
         else:
             await ctx.send(f":warning: **Invalid extension**. Valid extensions: `{list(self.bot.extensions.keys())}`")
+
+    @reload.error
+    async def settranslation_error(self, ctx, error):
+        if isinstance(error, CheckFailure):
+            await ctx.send("🔒 **You do not have permission to use this command**.")
 
 
 def setup(bot):
