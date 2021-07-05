790f8561592a791595ed0636387b86767a724fa4
galacticwarrior9 <zareef1@yahoo.com>
2021-07-05 15:10:52 +0100

    Fix hadith transmitter biographies being sent multiple times

--- hadith/transmitter_biographies.py
+++ hadith/transmitter_biographies.py
@@ -40,46 +40,45 @@ class Biographies(commands.Cog):
         em = discord.Embed(title='الذهبي - سير أعلام النبلاء', color=0x4aa807, description=pages[0])
         em.set_author(name=title)
 
+        if num_pages == 1:
+            return await ctx.send(embed=em)
+
         if num_pages > 1:
             page = 1
             em.set_footer(text=f'Page {page}/{num_pages}')
 
+        # If there are multiple pages, construct buttons for their navigation.
+        buttons = [
+            manage_components.create_button(style=ButtonStyle.green, label="الصفحة التالية", emoji="⬅",
+                                            custom_id="biography_next_page"),
+            manage_components.create_button(style=ButtonStyle.red, label="الصفحة السابقة", emoji="➡",
+                                            custom_id="biography_previous_page"),
+        ]
+        action_row = manage_components.create_actionrow(*buttons)
+        await ctx.send(embed=em, components=[action_row])
         while True:
-            if num_pages == 1:
-                return await ctx.send(embed=em)
+            try:
+                button_ctx = await manage_components.wait_for_component(self.bot, components=action_row,
+                                                                        timeout=600)
+                if button_ctx.custom_id == 'biography_previous_page':
+                    if page > 1:
+                        page -= 1
+                    else:
+                        page = num_pages
+                    em.description = pages[page - 1]
+                    em.set_footer(text=f'Page {page}/{num_pages}')
+                    await button_ctx.edit_origin(embed=em)
+                elif button_ctx.custom_id == 'biography_next_page':
+                    if page < num_pages:
+                        page += 1
+                    else:
+                        page = 1
+                    em.description = pages[page - 1]
+                    em.set_footer(text=f'Page {page}/{num_pages}')
+                    await button_ctx.edit_origin(embed=em)
 
-            # If there are multiple pages, construct buttons for their navigation.
-            buttons = [
-                manage_components.create_button(style=ButtonStyle.green, label="الصفحة التالية", emoji="⬅",
-                                                custom_id="biography_next_page"),
-                manage_components.create_button(style=ButtonStyle.red, label="الصفحة السابقة", emoji="➡",
-                                                custom_id="biography_previous_page"),
-            ]
-            action_row = manage_components.create_actionrow(*buttons)
-            await ctx.send(embed=em, components=[action_row])
-            while True:
-                try:
-                    button_ctx = await manage_components.wait_for_component(self.bot, components=action_row,
-                                                                            timeout=600)
-                    if button_ctx.custom_id == 'biography_previous_page':
-                        if page > 1:
-                            page -= 1
-                        else:
-                            page = num_pages
-                        em.description = pages[page - 1]
-                        em.set_footer(text=f'Page {page}/{num_pages}')
-                        await button_ctx.edit_origin(embed=em)
-                    elif button_ctx.custom_id == 'biography_next_page':
-                        if page < num_pages:
-                            page += 1
-                        else:
-                            page = 1
-                        em.description = pages[page - 1]
-                        em.set_footer(text=f'Page {page}/{num_pages}')
-                        await button_ctx.edit_origin(embed=em)
-
-                except asyncio.TimeoutError:
-                    break
+            except asyncio.TimeoutError:
+                break
 
     @commands.command(name="biography")
     async def biography(self, ctx, *, name):
