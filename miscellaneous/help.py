62e0ecc64b18c1138a36a6de763a0c74556a6bb0
galacticwarrior9 <zareef1@yahoo.com>
2022-04-26 00:23:33 +0100

    Document `tajweed` argument for `-rmushaf`

--- miscellaneous/help.py
+++ miscellaneous/help.py
@@ -59,7 +59,9 @@ class Help(commands.Cog):
                                                                  "\n\nAdd 'tajweed' to the end of the command for color-coded tajweed rules."
                                                                  f"\n\nExample: `{pre}mushaf 1:1 tajweed`")
 
-            em.add_field(name=f"{pre}rmushaf", inline=True, value="Gets a random page of a standard *mushaf*.")
+            em.add_field(name=f"{pre}rmushaf", inline=True, value="Gets a random page of a standard *mushaf*."
+                                                                  "\n\nAdd 'tajweed' to the end of the command for color-coded tajweed rules."
+                                                                  f"\n\nExample: `{pre}rmushaf tajweed`")
 
             em.add_field(name=f"{pre}surah", inline=True, value="Get information about a surah."
                                                                 f"\n\n`{pre}surah <surah number>`"
