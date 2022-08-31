a5be54eae3efafca51f5f61684f2b0b8797afe7a
galacticwarrior9 <zareef1@yahoo.com>
2022-08-31 16:50:05 +0100

    Cache guild translations

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -2,6 +2,8 @@ import asyncio
 import configparser
 
 import aiomysql
+from asyncache import cached
+from cachetools import TTLCache
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -16,8 +18,11 @@ user_prayer_times_table_name = config['MySQL']['user_prayer_times_table_name']
 
 loop = asyncio.get_event_loop()
 
+_translations_cache = TTLCache(maxsize=1024, ttl=600)
+
 
 class DBHandler:
+
     @classmethod
     async def create_connection(cls):
         connection = await aiomysql.connect(host=host, user=user, password=password, db=database,
@@ -25,6 +30,7 @@ class DBHandler:
         return connection
 
     @classmethod
+    @cached(_translations_cache)
     async def get_guild_translation(cls, guild_id):
         try:
             connection = await cls.create_connection()
@@ -48,6 +54,7 @@ class DBHandler:
 
     @classmethod
     async def update_guild_translation(cls, guild_id, translation):
+        _translations_cache.__setitem__(guild_id, translation)
         connection = await cls.create_connection()
         async with connection.cursor() as cursor:
             create_sql = f"INSERT INTO {server_translations_table_name} (server, translation) VALUES (%s, %s) " \
@@ -57,6 +64,7 @@ class DBHandler:
 
     @classmethod
     async def delete_guild_translation(cls, guild_id):
+        _translations_cache.pop(guild_id)
         connection = await cls.create_connection()
         async with connection.cursor() as cursor:
             delete_sql = f"DELETE FROM {server_translations_table_name} WHERE server=%s"
